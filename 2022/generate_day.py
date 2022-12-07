2d583bce13510d5d4c80e45a05d7cd732ca60a73
Nikhil Gupta <nikgupta06@gmail.com>
2022-12-07 01:02:37 -0600

    Add script to generate new day's files and start daily README update

---
+++ 2022/generate_day.py
@@ -0,0 +1,59 @@
+import re
+from datetime import datetime
+from pathlib import Path
+
+
+def main():
+    # Get current day
+    today = datetime.today()
+    # Create folder for day
+    day_folder = Path(str(today.day + 1).rjust(2, "0"))
+    day_folder.mkdir(exist_ok=False)
+
+    # Create empty files for example and problem input
+    with open(day_folder / "example.txt", "w") as f:
+        pass
+    with open(day_folder / "in.txt", "w") as f:
+        pass
+
+    # Create template Python files
+    content = """
+def main():
+    with open("in.txt", "r") as infile:
+        lines = [line.strip() for line in infile.readlines()]
+    
+    pass
+
+
+if __name__ == '__main__':
+    main()
+"""
+    with open(day_folder / "q1.py", "w") as f:
+        f.write(content)
+    with open(day_folder / "q2.py", "w") as f:
+        f.write(content)
+
+    # Get README.md contents
+    with open("README.md", "r") as f:
+        lines = f.readlines()
+
+    last_line = lines[-1]
+    tbd_pattern = re.compile(r"Day \d+: TBD")
+    tbd_line = re.findall(tbd_pattern, last_line)
+    # Hyperlink current day to folder
+    current_day_line = last_line.replace(tbd_line[0], f"[{tbd_line[0]}]({str(today.day + 1).rjust(2, '0')})")
+    # Create placeholder for next day
+    next_day_line = last_line.replace(tbd_line[0], f"Day {str(today.day + 2).rjust(2, '0')}: TBD")
+
+    # Adjust file contents
+    lines.pop(-1)
+    lines.append(current_day_line + "\n")
+    lines.append(next_day_line + "\n")
+
+    # Write contents to and update README.md
+    with open("README.md", "w") as f:
+        f.writelines(lines)
+
+
+if __name__ == '__main__':
+    main()
