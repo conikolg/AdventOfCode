68f8ba0da7bb688771c66e66161226a4d63c54a6
Nikhil Gupta <nikgupta06@gmail.com>
2023-12-06 23:51:24 -0600

    Add 2023 Day 7

---
+++ 2023/07/q1.py
@@ -0,0 +1,52 @@
+strength = "AKQJT98765432"
+
+
+def card_strength_tuple(cards: str):
+    return tuple(
+        20 - strength.index(c)
+        for c in cards
+    )
+
+
+def main():
+    with open("in.txt", "r") as infile:
+        lines = [line.strip() for line in infile.readlines()]
+
+    hands: list = [
+        line.split()
+        for line in lines
+    ]
+
+    for hand in hands:
+        counts = {e: hand[0].count(e) for e in set(hand[0])}
+        if 5 in counts.values():
+            hand.append(5)
+        elif 4 in counts.values():
+            hand.append(4)
+        elif 3 in counts.values():
+            if 2 in counts.values():
+                hand.append(3.5)
+            else:
+                hand.append(3)
+        elif 2 in counts.values():
+            if sorted(list(counts.values())) == [1, 2, 2]:
+                hand.append(2.5)
+            else:
+                hand.append(2)
+        else:
+            hand.append(1)
+
+        hand[1] = int(hand[1])
+        hand.append(card_strength_tuple(hand[0]))
+
+    hands.sort(key=lambda h: (h[2], h[3]))
+
+    answer = 0
+    for i, h in enumerate(hands):
+        print(h, '\t', i + 1, h[1], (i + 1) * h[1])
+        answer += (i + 1) * h[1]
+    print(answer)
+
+
+if __name__ == '__main__':
+    main()
